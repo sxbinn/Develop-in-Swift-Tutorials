617869e8edb6508c1c340d80d0b669705aa681f0
“sxbinn” <tnqls7307@gmail.com>
2026-04-14 03:08:55 +0900

    feat: add FriendsFavoriteMovies ch.3(data relationships)

---
+++ DataModeling/Ch3_ FriendsFavoriteMovies/Ch3_ FriendsFavoriteMovies/FilteredMovieList.swift
@@ -0,0 +1,18 @@
+//
+//  FilteredMovieList.swift
+//  Ch3_ FriendsFavoriteMovies
+//
+//  Created by kosoobin on 4/14/26.
+//
+
+import SwiftUI
+
+struct FilteredMovieList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FilteredMovieList()
+}
