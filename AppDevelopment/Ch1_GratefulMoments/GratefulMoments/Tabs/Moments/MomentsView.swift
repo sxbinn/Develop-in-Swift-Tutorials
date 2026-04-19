42d1a2777b81b5069539513751977c9c4cc5be86
“sxbinn” <tnqls7307@gmail.com>
2026-04-20 04:34:02 +0900

    feat: add app devlopment Ch1_GratefulMoments

---
+++ AppDevelopment/Ch1_GratefulMoments/GratefulMoments/Tabs/Moments/MomentsView.swift
@@ -0,0 +1,83 @@
+//
+//  MomentsView.swift
+//  GratefulMoments
+//
+//  Created by kosoobin on 4/20/26.
+//
+
+import SwiftUI
+import SwiftData
+
+struct MomentsView: View {
+    @State private var showCreateMoment = false
+    @Query(sort: \Moment.timestamp)
+    private var moments: [Moment]
+    
+    static let offsetAmount: CGFloat = 70.0
+    
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                pathItems
+                    .frame(maxWidth: .infinity)
+            }
+            .overlay {
+                if moments.isEmpty {
+                    ContentUnavailableView {
+                        Label("아직 게시된 내용이 없습니다!", systemImage: "exclamationmark.circle.fill")
+                    } description: {
+                        Text("감사의 마음을 담아 이 공간에 글이나 사진을 남겨보세요.")
+                    }
+                }
+            }
+            .toolbar {
+                ToolbarItem(placement: .primaryAction) {
+                    Button {
+                        showCreateMoment = true
+                    } label: {
+                        Image(systemName: "plus")
+                    }
+                    .sheet(isPresented: $showCreateMoment) {
+                        MomentEntryView()
+                    }
+                }
+            }
+            .defaultScrollAnchor(.bottom, for: .initialOffset)
+            .defaultScrollAnchor(.bottom, for: .sizeChanges)
+            .defaultScrollAnchor(.top, for: .alignment)
+            .navigationTitle("Grateful Moments")
+        }
+    }
+    
+    private var pathItems: some View {
+        ForEach(moments.enumerated(), id: \.0) { index, moment in
+            NavigationLink {
+                MomentDetailView(moment: moment)
+            } label: {
+                // 가장 최근 항목이 눈에 띄도록
+                if moment == moments.last {
+                    MomentHexagonView(moment: moment, layout: .large)
+                } else {
+                    MomentHexagonView(moment: moment)
+                        .offset(x: sin(Double(index) * .pi / 2) * Self.offsetAmount)
+                }
+            }
+            .scrollTransition { content, phase in
+                content
+                    .opacity(phase.isIdentity ? 1 : 0)
+                    .scaleEffect(phase.isIdentity ? 1: 0.8)
+            }
+        }
+    }
+}
+
+#Preview {
+    MomentsView()
+        .sampleDataContainer()
+}
+
+#Preview("No moments") {
+    MomentsView()
+        .modelContainer(for: [Moment.self])
+        .environment(DataContainer())
+}
